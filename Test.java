ac81563c5ad7b10a7a9560f41b587974e6ee1a65
dipak pawar <dbpawar877@gmail.com>
2023-09-25 22:20:03 +0530

    added sample java test program

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Test{
+
+	public static void main(String[] args){
+	System.ot.println(x:"This is java program")
+	System.ot.println(x:"Output from java program")
+	System.ot.println(new Date())
+  }
+}
