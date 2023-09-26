44434b2a21db335e2a71ddb73a08654cab1740d1
dipak pawar <dbpawar877@gmail.com>
2023-09-26 22:10:41 +0530

    added some changes

--- Test.java
+++ Test.java
@@ -1,10 +1,10 @@
 import java.util.Date;
 
 public class Test{
-
 	public static void main(String[] args){
-	System.ot.println(x:"This is java program")
-	System.ot.println(x:"Output from java program")
+	String x = "First_Jenkins";	
+	System.ot.println(x+"This is java program")
+	System.ot.println(x+"Output from java program")
 	System.ot.println(new Date())
   }
 }
